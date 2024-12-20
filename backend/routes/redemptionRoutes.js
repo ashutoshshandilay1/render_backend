82c07824ddbce6591f2b1b006d3315b9e617f3cc
Ashutosh shandilay <161586271+ashutoshshandilay1@users.noreply.github.com>
2024-12-20 13:25:48 +0530

    Delete backend directory

--- backend/routes/redemptionRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createRedemptionRequest } from '../controllers/redemptionController.js';
-
-const router = express.Router();
-
-router.post('/', createRedemptionRequest);
-
-export default router;
